714884dc8ea590cde7bd7ae234093ecb07803607
Nicolas Santiago Naranjo <nsnaranjo6@misena.edu.co>
2020-10-27 23:32:05 -0500

    Taller algoritmos. Operaciones en C#

---
+++ AddProduct.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TallerAlgoritmos.BusinessLayer;
+
+namespace TallerAlgoritmos
+{
+    class AddProduct
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0; 
+            int num2 = 0;
+            int result = 0;
+
+            Console.WriteLine("Operaciones básicas\r");
+            Console.WriteLine("-------------------\n");
+
+
+            Console.Write("Ingrese el primer número ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo número ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Elija una opción:");
+            Console.WriteLine("\ts - Sumar");
+            Console.WriteLine("\tm - Multiplicar");
+            Console.Write("Opción ");
+
+            switch (Console.ReadLine())
+            {
+                case "s":
+                    result = BusinessLayer.AddProduct.Sumar(num1, num2);
+                    Console.WriteLine($"Resultado: {num1} + {num2} = " + result);
+                    break;
+
+                case "m":
+                    result = BusinessLayer.AddProduct.Multiplicar(num1, num2);
+                    Console.WriteLine($"Resultado: {num1} + {num2} = " + result);
+                    break;
+            }
+
+            Console.Write("Presione cualquier tecla para salir del programa");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ TallerAlgoritmos.BusinessLayer/Operations.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TallerAlgoritmos.BusinessLayer
+{
+    static public class AddProduct
+    {
+        /// <summary>
+        /// Suma de dos números enteros
+        /// </summary>
+        /// <param name="var1">Primer número entero</param>
+        /// <param name="var2">Segundo número entero</param>
+        /// <returns>Resultado de sumar los dos números</returns>
+        public static int Sumar(int var1, int var2)
+        {
+            int result = var1 + var2;
+            
+            return result;
+        }
+
+        /// <summary>
+        /// Multiplicación de dos números
+        /// </summary>
+        /// <param name="var1">Primer número entero</param>
+        /// <param name="var2">Segundo número entero</param>
+        /// <returns>Resultado de multiplicar los dos números</returns>
+        public static int Multiplicar(int var1, int var2)
+        {
+            int result = var1 * var2;
+
+            return result;
+        }
+    }
+
+    static public class CipherInversion
+    {
+        public static int Invertir(int var1)
+        {
+
+        }
+
+            
+    }
+
+
+}
